feca6ac5598b79d33ba65570ced8a9b66d846e75
Stephen Kurshner <skurshner@gmail.com>
2021-12-08 15:18:08 -0800

    debug - hardcoded db into knexfile

--- back-end/knexfile.js
+++ back-end/knexfile.js
@@ -4,14 +4,14 @@
  * You will not need to make changes to this file.
  */
 
-require('dotenv').config();
+require("dotenv").config();
 const path = require("path");
 
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_DEVELOPMENT = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_TEST = "postgresql://postgres@localhost/postgres",
-  DATABASE_URL_PREVIEW = "postgresql://postgres@localhost/postgres",
+  DATABASE_URL = "postgres://lrcpelnp:EVMvIVtkkdvKn9IOrnGw5u_e52hH8Bm-@kashin.db.elephantsql.com/lrcpelnp",
+  DATABASE_URL_DEVELOPMENT = "postgres://ogfnknef:enQZicD71ewLqgWih3VxFiWHV2Kd7Wmo@kashin.db.elephantsql.com/ogfnknef",
+  DATABASE_URL_TEST = "postgres://nporvxco:5DleknX_G40dvcLOM29aJvQJ9ZDvGV7q@kashin.db.elephantsql.com/nporvxco",
+  DATABASE_URL_PREVIEW = "postgres://azaxzoyb:AB_ggrzQijXJfwQeZPW4mWyGmbJIaSNy@kashin.db.elephantsql.com/azaxzoyb",
   DEBUG,
 } = process.env;
 
