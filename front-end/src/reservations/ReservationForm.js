2445608bab5b4a73b24fbbdc1c9a236b8896bdc1
Stephen Kurshner <skurshner@gmail.com>
2021-12-08 14:55:48 -0800

    removed .env.development & env.production from .gitignore

--- front-end/src/reservations/ReservationForm.js
+++ front-end/src/reservations/ReservationForm.js
@@ -58,7 +58,7 @@ const ReservationForm = () => {
           <label htmlFor="mobile_number" className="form-label">
             Mobile Number
           </label>
-          <div class="form-text mx-2">Use format 000-000-0000</div>
+          <div className="form-text mx-2">Use format 000-000-0000</div>
         </div>
         <div className="form-floating mb-3">
           <input
