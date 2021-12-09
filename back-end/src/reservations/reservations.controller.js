479028ac8fc2d909cb7d10e5868e187fe05e3cb5
Stephen Kurshner <skurshner@gmail.com>
2021-12-09 12:48:36 -0800

    added valitation for first_name, last_name and mobile number

--- back-end/src/reservations/reservations.controller.js
+++ back-end/src/reservations/reservations.controller.js
@@ -2,6 +2,53 @@ const service = require("./reservations.service");
 const asyncErrorBoundary = require("../errors/asyncErrorBoundary");
 
 /**
+ * Middleware to check that body has data
+ */
+const hasData = (req, res, next) => {
+  if (req.body.data) {
+    return next();
+  }
+  next({ status: 400, message: "body must have data property" });
+};
+
+/**
+ * Middleware for post body validation
+ */
+const hasFirstName = (req, res, next) => {
+  const { data: { first_name } = {} } = req.body;
+  if (first_name && first_name !== "") {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "A 'first_name' is required",
+  });
+};
+
+const hasLastName = (req, res, next) => {
+  const { data: { last_name } = {} } = req.body;
+  if (last_name && last_name !== "") {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "A 'last_name' is required",
+  });
+};
+
+const hasMobileNumber = (req, res, next) => {
+  const { data: { mobile_number } = {} } = req.body;
+  const validPhoneNumber = /^\(?[0-9]{3}\)?[-]{1}?[0-9]{3}[-]{1}?[0-9]{4}$/;
+  if (mobile_number && mobile_number.match(validPhoneNumber)) {
+    return next();
+  }
+  next({
+    status: 400,
+    message: "A valid 'mobile_number' is required",
+  });
+};
+
+/**
  * List handler for reservation resources
  */
 const list = async (req, res) => {
@@ -20,6 +67,12 @@ const create = async (req, res) => {
 };
 
 module.exports = {
-  create: asyncErrorBoundary(create),
+  create: [
+    hasData,
+    hasFirstName,
+    hasLastName,
+    hasMobileNumber,
+    asyncErrorBoundary(create),
+  ],
   list: asyncErrorBoundary(list),
 };
