dc2316c245ff1c8f709f612ae95a87b6e487660c
Stephen Kurshner <skurshner@gmail.com>
2021-12-15 11:32:46 -0800

    added route and components for table creation

---
+++ front-end/src/tables/CreateTable.js
@@ -0,0 +1,12 @@
+import React from "react";
+import TableForm from "./TableForm";
+
+const CreateTable = () => {
+  return (
+    <>
+      <TableForm />
+    </>
+  );
+};
+
+export default CreateTable;
---
+++ front-end/src/tables/TableForm.js
@@ -0,0 +1,60 @@
+import React from "react";
+import { useHistory } from "react-router-dom";
+import ErrorAlert from "../layout/ErrorAlert";
+
+const TableForm = ({ error }) => {
+  const history = useHistory();
+
+  const handleCancel = () => history.goBack();
+
+  return (
+    <div className="container">
+      {/* <ErrorAlert error={error} /> */}
+      <h1>Create a New Table</h1>
+      <form>
+        <div className="form-floating mb-3">
+          <input
+            type="text"
+            name="table_name"
+            className="form-control"
+            id="table_name"
+            autoComplete={"off"}
+            placeholder={"Table Name"}
+            required
+          />
+          <label htmlFor="table_name" className="form-label">
+            Table name
+          </label>
+        </div>
+
+        <div className="form-floating mb-3">
+          <input
+            type="number"
+            name="capacity"
+            className="form-control"
+            id="capacity"
+            placeholder="1"
+            required
+          />
+          <label htmlFor="capacity" className="form-label">
+            Capacity
+          </label>
+        </div>
+        <div className="row">
+          <div className="col-auto pr-0 mr-2">
+            <button className="btn btn-secondary" onClick={handleCancel}>
+              Cancel
+            </button>
+          </div>
+          <div className="col-auto px-0">
+            <button className="btn btn-primary" type="submit">
+              Submit
+            </button>
+          </div>
+        </div>
+      </form>
+    </div>
+  );
+};
+
+export default TableForm;
