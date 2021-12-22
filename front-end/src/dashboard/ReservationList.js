e498fa7969c7dac32ef9cebfcaa604f873784352
Stephen Kurshner <skurshner@gmail.com>
2021-12-21 21:41:53 -0800

    styled reservation list and list items

--- front-end/src/dashboard/ReservationList.js
+++ front-end/src/dashboard/ReservationList.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReservationListItem from "./ReservationListItem";
 import "./ReservationList.css";
+import { useHistory } from "react-router-dom";
 
 const ReservationList = ({
   date = "",
@@ -11,6 +12,7 @@ const ReservationList = ({
   todayButtonHandler,
   nextDayButtonHandler,
 }) => {
+  const history = useHistory();
   const reservationHeader = () => {
     if (!reservations.length) {
       return `No Reservations for ${date}`;
@@ -23,17 +25,17 @@ const ReservationList = ({
 
   const showSelectDateButtons = () => {
     return (
-      <div className="date-buttons d-flex flex-row">
+      <div className="d-flex flex-row">
         <button
           type="button"
-          className="btn btn-outline-primary flex-sm-grow-0 flex-grow-1"
+          className="btn btn-outline-primary me-1 flex-sm-grow-0 flex-grow-1"
           onClick={() => previousDayButtonHandler()}
         >
           Prev
         </button>
         <button
           type="button"
-          className="btn btn-outline-primary flex-sm-grow-0 flex-grow-1"
+          className="btn btn-outline-primary me-1 flex-sm-grow-0 flex-grow-1"
           onClick={() => todayButtonHandler()}
         >
           Today
@@ -51,29 +53,35 @@ const ReservationList = ({
 
   return (
     <div className="reservation-container p-3">
-      <div className="row d-flex flex-column flex-sm-row flex-wrap">
+      <div className="row d-flex flex-column flex-sm-row flex-wrap mb-3">
         <div className="col col-sm-7 d-md-flex flex-column align-items-start mb-2">
           <h2>{!search && reservationHeader()}</h2>
           {!search && showSelectDateButtons()}
           <h3>{search && !reservations.length && "No reservations found"}</h3>
         </div>
         <div className="col col-sm-5 d-flex justify-content-sm-end align-items-sm-start mt-2 mt-sm-0">
-          <button className="btn btn-primary flex-fill flex-sm-grow-0">
-            <i class="bi bi-plus-square me-2"></i>New Reservation
+          <button
+            type="button"
+            name="New Reservation"
+            className="btn btn-primary flex-fill flex-sm-grow-0"
+            onClick={() => history.push("/reservations/new")}
+          >
+            <i class="bi bi-plus-circle-fill me-2"></i>New Reservation
           </button>
         </div>
       </div>
-
-      {reservations.map(reservation => {
-        return (
-          <div key={reservation.reservation_id}>
-            <ReservationListItem
-              reservation={reservation}
-              cancelButtonClickHandler={cancelButtonClickHandler}
-            />
-          </div>
-        );
-      })}
+      <div>
+        {reservations.map(reservation => {
+          return (
+            <div key={reservation.reservation_id}>
+              <ReservationListItem
+                reservation={reservation}
+                cancelButtonClickHandler={cancelButtonClickHandler}
+              />
+            </div>
+          );
+        })}
+      </div>
     </div>
   );
 };
--- front-end/src/dashboard/ReservationListItem.js
+++ front-end/src/dashboard/ReservationListItem.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./ReservationListItem.css";
 
 const ReservationListItem = ({ reservation, cancelButtonClickHandler }) => {
   const reservation_id = reservation.reservation_id;
@@ -17,61 +18,73 @@ const ReservationListItem = ({ reservation, cancelButtonClickHandler }) => {
   };
 
   return (
-    <div className="card d-flex flex-row py-3 mb-3">
-      <div className="mx-3">
-        <p>
-          <strong>Name: </strong>
-          {reservation.first_name} {reservation.last_name}
-        </p>
+    <div className="card d-flex flex-row flex-wrap justify-content-between p-3 p-md-4 mb-1">
+      <div className="col-12 col-md-6 col-xl-8 d-flex flex-row flex-wrap">
+        <div className="d-flex align-items-xl-center col-5 col-sm-4 col-xl-2 pb-2 p-xl-0">
+          <p className="p-bold">
+            <i class="bi bi-clock me-2"></i>
+            {formatTime(reservation.reservation_time)}
+          </p>
+        </div>
+        <div className="d-flex align-items-xl-center col-7 col-sm-8 col-xl-4">
+          <p>
+            <i class="bi bi-person me-2"></i>
+            {reservation.first_name} {reservation.last_name} (
+            {reservation.people})
+          </p>
+        </div>
+        <div className="d-flex align-items-xl-center col-5 col-sm-4 col-xl-3 order-xl-3">
+          <p data-reservation-id-status={reservation_id}>
+            <i class="bi bi-clipboard-check me-2"></i>
+            {reservation.status[0].toUpperCase() +
+              reservation.status.substring(1)}
+          </p>
+        </div>
+        <div className="d-flex align-items-xl-center col-7 col-sm-8 col-xl-3">
+          <p>
+            <i class="bi bi-telephone me-2"></i>
+            {reservation.mobile_number}
+          </p>
+        </div>
       </div>
-      <div className="mx-3">
-        <p>
-          <strong>Number: </strong>
-          {reservation.mobile_number}
-        </p>
-      </div>
-      <div className="mx-3">
-        <p>
-          <strong>Time: </strong>
-          {formatTime(reservation.reservation_time)}
-        </p>
-      </div>
-      <div className="mx-3">
-        <p>
-          <strong>People in Party: </strong>
-          {reservation.people}
-        </p>
-      </div>
-      <div className="mx-3">
-        <p data-reservation-id-status={reservation_id}>
-          <strong>Status: </strong>
-          {reservation.status}
-        </p>
-      </div>
-      <div className="mx-3">
-        {reservation.status === "booked" && (
+
+      {reservation.status === "booked" && (
+        <div className="col-12 col-md-6 col-xl-4 d-flex flex-row align-items-start align-items-md-center mt-3 mt-md-0">
           <button
+            type="button"
+            name="cancel"
+            className="btn btn-text cancel col-4 col-sm-3 col-md-4 text-nowrap pe-2 pe-md-1"
             data-reservation-id-cancel={reservation.reservation_id}
             onClick={() => cancelButtonClickHandler(reservation_id)}
           >
             Cancel
           </button>
-        )}
-      </div>
-      <div className="mx-3">
-        {reservation.status === "booked" && (
-          <Link to={`/reservations/${reservation_id}/edit`}>
-            <button>Edit</button>
+          <Link
+            to={`/reservations/${reservation_id}/edit`}
+            className="d-flex col-4 col-sm-4  px-2 px-md-1"
+          >
+            <button
+              type="button"
+              name="edit"
+              className="btn btn-outline-primary flex-grow-1"
+            >
+              Edit
+            </button>
           </Link>
-        )}
-      </div>
-      <div className="mx-3">
-        {reservation.status === "booked" && (
-          <Link to={`/reservations/${reservation_id}/seat`}>
-            <button>Seat</button>
+          <Link
+            to={`/reservations/${reservation_id}/seat`}
+            className="d-flex col-4 col-sm-5 col-md-4 ps-2 ps-md-1"
+          >
+            <button
+              type="button"
+              name="seat"
+              className="btn btn-primary flex-grow-1"
+            >
+              Seat
+            </button>
           </Link>
-        )}
-      </div>
+        </div>
+      )}
     </div>
   );
 };
